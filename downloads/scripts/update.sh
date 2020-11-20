0feb809efa704ddfde0147981e28063e1dc581d7
jeremysmai <contact@phantominc.net>
2020-11-19 20:29:06 -0600

    changed update.sh and downloads page reflects that

--- downloads/scripts/update.sh
+++ downloads/scripts/update.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # Update.sh
-#Version 01
-#Phantom Inc BB 2019
+#Version 03
+#Phantom Inc BB 2020
 clear
-echo -e "\033[8m Update Hidden line :D PhantomInc2019 \033[m"
+echo -e "\033[8m Update Hidden line :D PhantomInc2020 \033[m"
 echo -e "\033[01;30m	      //////// /// /// /////// ///  /// ////// /////// ///   //// "
 echo -e "             ///  /// /// /// /// /// //// ///  ///   /// /// //// /////  "
 echo -e "            //////// /////// /////// /// ////  ///   /// /// /// // ///   "
 echo -e "           ///      /// /// /// /// ///  ///  ///   /////// ///    ///    "
-echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m updating... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2019 \033[m"
+echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m updating... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2020 \033[m"
 echo -e "\033[8m Anotha Hidden line :D enjoying my updater? \033[m"
 
 sudo apt-get -y update
-echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m upgrading... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2019 \033[m"
+echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m upgrading... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2020 \033[m"
 
 sudo apt-get -y upgrade
 
-echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m checking dist upgrade... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2019 \033[m"
+echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m checking dist upgrade... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2020 \033[m"
 
 sudo apt-get -y dist-upgrade
 
-echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m cleaning in process... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2019 \033[m"
+echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m cleaning in process... please wait... \033[m      \033[2m \033[3m \033[01;30m Inc. 2020 \033[m"
 
-sudo apt-get --purge -y autoremove
+sudo apt --purge -y autoremove
+
+echo -e "          ///  \033[00m  \033[5m \033[3m \033[7m ~ Update Complete ~ \033[m      \033[2m \033[3m \033[01;30m Inc. 2020 \033[m"
 
 exit 0
